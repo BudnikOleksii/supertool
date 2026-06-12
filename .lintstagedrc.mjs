c33672fc7598638c70a603ef4c900db6648259bc
Oleksii Budnik <budnikoleksii@gmail.com>
2026-06-12 07:35:19 +0300

    Revert "Update .lintstagedrc.mjs"
    This reverts commit d0e6a41438bfe2c062bef4ab77543ab878fb9f31.

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -6,16 +6,8 @@ const buildCommandList = (fileList, commandList) => {
   if (targetList.length === EMPTY_LENGTH) {
     return [];
   }
-const quoteForShell = (filePath) => `'${filePath.replaceAll("'", "'\\''")}'`;
-
-const buildCommandList = (fileList, commandList) => {
-  const targetList = fileList.filter((file) => !file.includes(GENERATED_CLIENT_DIR));
-  if (targetList.length === EMPTY_LENGTH) {
-    return [];
-  }
-  const files = targetList.map(quoteForShell).join(' ');
+  const files = targetList.join(' ');
   return commandList.map((command) => `${command} ${files}`);
-};
 };
 
 const lintStagedConfig = {
